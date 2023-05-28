c635af091f2c53be4e42e9e7ddb8520fa8e67181
MaiLF <Mailf.channel@gmail.com>
2023-05-29 02:47:09 +0300

    add prod json logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-require(`dotenv-defaults`).config();
+import * as dotenv from "dotenv-defaults";
+dotenv.config();
 
-import logger from "./logger";
 import { CallbackService } from "vk-io";
+import * as winston from "winston";
+import logger from "./logger";
 import * as services from "./services";
 
 async function main() {
+  if (process.env.NODE_ENV !== "production") {
+    logger.add(
+      new winston.transports.Console({
+        format: winston.format.combine(winston.format.prettyPrint()),
+      })
+    );
+  } else {
+    logger.add(new winston.transports.Console());
+  }
+
   const vkCredentials = {
     login: process.env.VK_LOGIN,
     password: process.env.VK_PASSWORD,
--- src/logger.ts
+++ src/logger.ts
@@ -3,14 +3,9 @@ import * as winston from "winston";
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL,
   format: winston.format.combine(
-    winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.printf((info) => {
-      const _dt = new Date(info.timestamp);
-      return `[${_dt.toLocaleString()}] ${info.level} ${info.message}`;
-    })
+    winston.format.json()
   ),
 });
-logger.add(new winston.transports.Console());
 
 export default logger;
